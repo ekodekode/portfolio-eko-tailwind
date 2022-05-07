bd8168b07b7f93bdf93ec3cc41d88c6b6a0f96be
ekodekode <prazex@google.com>
2022-05-07 16:25:29 +0700

    test

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        violet: '#7c3aed',
+        hitam: '#171717',
+      },
+      screen: {
+        '2x1': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
