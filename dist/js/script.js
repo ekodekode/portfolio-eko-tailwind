4b74392e41ebfffc58490e8c08546d4222b0c3d4
ekodekode <104394868+ekodekode@users.noreply.github.com>
2022-05-14 21:35:15 +0700

    Add files via upload

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,17 @@
 window.onscroll = function () {
   const header = document.querySelector('header');
   const fixedNav = header.offsetTop;
-
+  // t.top
+  const tTop = document.querySelector('#top');
+  // t.top akhr
   if (window.pageYOffset > fixedNav) {
     header.classList.add('navbar-fixed');
+    tTop.classList.remove('hidden');
+    tTop.classList.add('flex');
   } else {
     header.classList.remove('navbar-fixed');
+    tTop.classList.remove('flex');
+    tTop.classList.add('hidden');
   }
 };
 
@@ -18,3 +24,24 @@ hamburger.addEventListener('click', function () {
   hamburger.classList.toggle('hamburger-active');
   navMenu.classList.toggle('hidden');
 });
+
+// dark mode
+
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function () {
+  if (darkToggle.checked) {
+    html.classList.add('dark');
+    localStorage.theme = 'dark';
+  } else {
+    html.classList.remove('dark');
+    localStorage.theme = 'light';
+  }
+});
+
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
